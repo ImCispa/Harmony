f2acf32cdb317bebf4fd783b496adb274ecff0a0
Christian Perico <christian.perico.1997@gmail.com>
2024-12-01 21:16:42 +0100

    #2 - refactored input in api, validation method and output to print out the types, all for user

--- backend/modules/user/handlers.go
+++ backend/modules/user/handlers.go
@@ -2,7 +2,6 @@ package user
 
 import (
 	"harmony/internal/database"
-	"harmony/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -23,28 +22,28 @@ func NewHandler(db *database.Service) *Handler {
 }
 
 func (h *Handler) Create(c *gin.Context) {
-	var user User
+	type RequestBody struct {
+		Name string `json:"name"`
+		Mail string `json:"mail"`
+	}
+	var rb RequestBody
 
 	// validate input
-	if err := c.ShouldBindJSON(&user); err != nil {
+	if err := c.ShouldBindJSON(&rb); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	if len(user.Name) == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Name required"})
+	if check, errMsg := IsNameValid(rb.Name); !check {
+		c.JSON(http.StatusBadRequest, gin.H{"error": errMsg})
 		return
 	}
-	if len(user.Mail) == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Mail required"})
-		return
-	}
-	if !utils.IsValidEmail(user.Mail) {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Mail not valid"})
+	if check, errMsg := IsMailValid(rb.Mail); !check {
+		c.JSON(http.StatusBadRequest, gin.H{"error": errMsg})
 		return
 	}
 
 	// check mail to see if already used
-	isMailUsed, err := h.Repo.IsMailUsed(user.Mail)
+	isMailUsed, err := h.Repo.IsMailUsed(rb.Mail)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Falied"})
 		return
@@ -55,19 +54,15 @@ func (h *Handler) Create(c *gin.Context) {
 	}
 
 	// create
+	user := NewUser(rb.Name, rb.Mail)
+
 	err = h.Repo.Create(&user)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
 		return
 	}
 
-	c.JSON(http.StatusCreated, gin.H{
-		"id":          user.ID,
-		"name":        user.Name,
-		"mail":        user.Mail,
-		"unique_name": user.UniqueName,
-		"servers":     user.Servers,
-	})
+	c.JSON(http.StatusCreated, user.Print())
 }
 
 func (h *Handler) Read(c *gin.Context) {
@@ -87,17 +82,15 @@ func (h *Handler) Read(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"id":          user.ID,
-		"name":        user.Name,
-		"mail":        user.Mail,
-		"unique_name": user.UniqueName,
-		"servers":     user.Servers,
-	})
+	c.JSON(http.StatusOK, user.Print())
 }
 
 func (h *Handler) Update(c *gin.Context) {
 	id := c.Param("id")
+	type RequestBody struct {
+		Name string `json:"name"`
+	}
+	var rb RequestBody
 
 	// validate input
 	objectId, err := primitive.ObjectIDFromHex(id)
@@ -105,9 +98,12 @@ func (h *Handler) Update(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	var in User
-	if err := c.ShouldBindJSON(&in); err != nil {
+	if err := c.ShouldBindJSON(&rb); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if check, errMsg := IsNameValid(rb.Name); !check {
+		c.JSON(http.StatusBadRequest, gin.H{"error": errMsg})
 		return
 	}
 
@@ -119,7 +115,7 @@ func (h *Handler) Update(c *gin.Context) {
 	}
 
 	// update data
-	user.Name = in.Name
+	user.Name = rb.Name
 
 	err = h.Repo.Update(*user)
 	if err != nil {
@@ -127,13 +123,7 @@ func (h *Handler) Update(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"id":          user.ID,
-		"name":        user.Name,
-		"mail":        user.Mail,
-		"unique_name": user.UniqueName,
-		"servers":     user.Servers,
-	})
+	c.JSON(http.StatusOK, user.Print())
 }
 
 func (h *Handler) Delete(c *gin.Context) {
@@ -152,7 +142,6 @@ func (h *Handler) Delete(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete user"})
 		return
 	}
-
 	if !isDeleted {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Failed to retreive user"})
 		return
--- backend/modules/user/models.go
+++ backend/modules/user/models.go
@@ -2,6 +2,8 @@ package user
 
 import (
 	"fmt"
+	"harmony/utils"
+	"regexp"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
@@ -14,9 +16,49 @@ type User struct {
 	Servers    []string           `bson:"servers"`
 }
 
-func (u *User) GenerateUniqueName(code int) string {
+func NewUser(name string, mail string) User {
+	return User{
+		Name: name,
+		Mail: mail,
+	}
+}
+
+func IsNameValid(name string) (bool, string) {
+	if len(name) == 0 {
+		return false, "Name is empty"
+	}
+	var pattern = regexp.MustCompile(`^[a-zA-Z0-9_]+$`)
+	if !pattern.MatchString(name) {
+		return false, "Name contains not allowed caracters"
+	}
+	return true, ""
+}
+
+func IsMailValid(mail string) (bool, string) {
+	if len(mail) == 0 {
+		return false, "Mail is empty"
+	}
+	if !utils.IsValidEmail(mail) {
+		return false, "Mail is not in the right format"
+	}
+	return true, ""
+}
+
+func (u *User) GenerateUniqueName(code int) {
+	if u.ID != primitive.NilObjectID {
+		return
+	}
 	u.UniqueName = fmt.Sprintf("%s:%04d", u.Name, code)
-	return u.UniqueName
+}
+
+func (u *User) Print() map[string]any {
+	return map[string]any{
+		"id":          u.ID,
+		"name":        u.Name,
+		"mail":        u.Mail,
+		"unique_name": u.UniqueName,
+		"servers":     u.Servers,
+	}
 }
 
 type UserCode struct {
